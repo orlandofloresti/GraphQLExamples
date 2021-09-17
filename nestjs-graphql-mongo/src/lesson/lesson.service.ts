afc94d0b65b717e079386c405e56363365a3104e
orflores <orflores@rassini.com>
2021-09-17 17:30:31 -0500

    Add example using nest and graphql

---
+++ nestjs-graphql-mongo/src/lesson/lesson.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Lesson } from './lesson.entity';
+import { v4 as uuid } from 'uuid';
+import { CreateLessonInput } from './lesson.input';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson)
+    private lessonRespository: Repository<Lesson>,
+  ) {}
+
+  async getLesson(id): Promise<Lesson> {
+    return this.lessonRespository.findOne({ id });
+  }
+
+  async getLessons(): Promise<Lesson[]> {
+    return this.lessonRespository.find();
+  }
+
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
+    const lesson = this.lessonRespository.create({
+      id: uuid(),
+      name,
+      startDate,
+      endDate,
+    });
+
+    return this.lessonRespository.save(lesson);
+  }
+}
